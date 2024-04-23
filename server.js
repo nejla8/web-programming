0ad0b1211300f2277692c5c927c12ce88d4dae75
Nejla S <112932190+nejla8@users.noreply.github.com>
2024-04-23 08:57:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
 //
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+// Create MySQL connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'nejla198',
+    database: 'mydatabase'
+});
+
+// Connect to MySQL
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+});
+
+// Fetch users from the database
+app.get('/users', (req, res) => {
+    let sql = 'SELECT * FROM users';
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+
+const PORT = 3306;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
