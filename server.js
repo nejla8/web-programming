ec171967b652e2e8aa99b8f7eb4fc85b9fc14c38
Nejla S <112932190+nejla8@users.noreply.github.com>
2024-06-04 00:48:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const db = mysql.createConnection({
     database: 'mydatabase'
 });
 
+const mysql = require('mysql');
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+});
+
+
 // Connect to MySQL
 db.connect((err) => {
     if (err) throw err;
@@ -20,21 +28,25 @@ db.connect((err) => {
 });
 
 // Define middleware for JWT authentication
+const jwt = require('jsonwebtoken');
+
 const authenticateJWT = (req, res, next) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization; 
     if (!token) {
-        res.sendStatus(401);
-    } else {
-        jwt.verify(token, 'your_secret_key', (err, decoded) => {
-            if (err) {
-                res.sendStatus(403);
-            } else {
-                req.user = decoded;
-                next();
-            }
-        });
+        return res.status(401).json({ error: 'Unauthorized' });
     }
+
+    jwt.verify(token, 'j43Jc#hD78!92mQP9aLs6Fy$xZ@tVbE&', (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: 'Forbidden' });
+        }
+
+        req.user = decoded;
+        next(); 
+    });
 };
+
+app.use(authenticateJWT);
 
 // Define routes
 app.get('/users', authenticateJWT, (req, res) => {
@@ -49,3 +61,17 @@ const PORT = 3306;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+
